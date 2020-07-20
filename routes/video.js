a324fd0a8d0be86f968b4ef859d65bf55e5bfbe8
JayPTucker <jayptuckerimportant@gmail.com>
2020-07-20 12:07:12 -0500

    Made a Change in the Video.js section with ffmpeg

--- routes/video.js
+++ routes/video.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
-var ffmpeg = require('fluent-ffmpeg')
+var ffmpeg = require('ffmpeg')
 
 const { Video } = require("../models/Video");
 
