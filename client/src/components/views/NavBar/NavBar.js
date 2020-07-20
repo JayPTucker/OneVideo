05af8908885510b8ceaddde4b0ca793752269297
JayPTucker <jayptuckerimportant@gmail.com>
2020-07-20 16:39:16 -0500

    Adjusted Navbar

--- client/src/components/views/NavBar/NavBar.js
+++ client/src/components/views/NavBar/NavBar.js
@@ -1,52 +1,47 @@
 import React, { useState } from 'react';
 import LeftMenu from './Sections/LeftMenu';
-import RightMenu from './Sections/RightMenu';
+import LoginSection from './Sections/LoginSection';
+
+import { Container, Row, Col } from "react-bootstrap";
 import { Drawer, Button, Icon } from 'antd';
-import './Sections/Navbar.css';
+// import './Sections/Navbar.css';
 
 function NavBar() {
-  const [visible, setVisible] = useState(false)
-
-  const showDrawer = () => {
-    setVisible(true)
-  };
-
-  const onClose = () => {
-    setVisible(false)
-  };
-
   return (
-    <nav className="menu" style={{ position: 'fixed', zIndex: 5, width: '100%' }}>
-      <div className="menu__logo">
-        <a href="/">Logo</a>
-      </div>
-      <div className="menu__container">
-        <div className="menu_left">
+    <Container fluid>
+      <Row>
+        <Col>
+          <a className="menu__logo" href="/">OneVideo</a>
+        </Col>
+
+        <Col>
           <LeftMenu mode="horizontal" />
-        </div>
-        <div className="menu_rigth">
-          <RightMenu mode="horizontal" />
-        </div>
-        <Button
-          className="menu__mobile-button"
-          type="primary"
-          onClick={showDrawer}
-        >
-          <Icon type="align-right" />
-        </Button>
-        <Drawer
-          title="Basic Drawer"
-          placement="right"
-          className="menu_drawer"
-          closable={false}
-          onClose={onClose}
-          visible={visible}
-        >
-          <LeftMenu mode="inline" />
-          <RightMenu mode="inline" />
-        </Drawer>
-      </div>
-    </nav>
+        </Col>
+
+        <Col>
+          <LoginSection mode="horizontal" />
+        </Col>
+      </Row>
+    </Container>
+    // <nav className="menu" style={{ position: 'fixed', zIndex: 5, width: '100%' }}>
+    //   <div className="menu__logo">
+    //     <a href="/">OneVideo</a>
+    //   </div>
+    //   <div className="menu__container">
+    //     <div className="menu_left">
+    //       <LeftMenu mode="horizontal" />
+    //     </div>
+    //     <div className="menu_rigth">
+    //       <LoginSection mode="horizontal" />
+    //     </div>
+    //     <Button
+    //       className="menu__mobile-button"
+    //       type="primary"
+    //     >
+    //       <Icon type="align-right" />
+    //     </Button>
+    //   </div>
+    // </nav>
   )
 }
 
