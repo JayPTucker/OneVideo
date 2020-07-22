79ffd4fd56e71cab749bc2acb5418309b38cf6be
Jay Paul Tucker <jayptuckerimportant@gmail.com>
2020-07-22 16:16:31 -0500

    Deleted .env

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ uploadFile('twitter-logo.png');
 
 
 const mongoose = require("mongoose");
-const connect = mongoose.connect(process.env.mongoURI,
+const connect = mongoose.connect(process.env.MONGO_URI,
   {
     useNewUrlParser: true, useUnifiedTopology: true,
     useCreateIndex: true, useFindAndModify: false
